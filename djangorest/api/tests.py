e0ae9d980aefcd0b2606437aea6a9110ae2f9c42
Kamara Deo <kamaradeo@Kamara-Deo.local>
2018-05-26 15:23:22 +0300

    [Feature) Initial setup

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,9 +1,30 @@
-from django.test import TestCase
-from .models import Shoppinglist
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.core.urlresolvers import reverse
+from django.test import TestCase
+from .models import User,Shoppinglist
 
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the bucketlist model."""
 
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.shoppinglist_name = "Write world class code"
+        self.shoppinglist = Shoppinglist(name=self.shoppinglist_name)
 
-# Create your tests here.
+    def test_model_can_create_a_bucketlist(self):
+        """Test the bucketlist model can create a bucketlist."""
+        old_count = Shoppinglist.objects.count()
+        self.shoppinglist.save()
+        new_count = Shoppinglist.objects.count()
+        self.assertNotEqual(old_count, new_count)
+    
+    def test_api_can_create_a_bucketlist(self):
+        """Test the api has bucket creation capability."""
+        self.client = APIClient()
+        self.shoppinglist_data = {'name': 'shirts'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.shoppinglist_data,
+            format="json")
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ShoppinglistSerializer
+from .models import Shoppinglist, User
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Shoppinglist.objects.all()
+    serializer_class = ShoppinglistSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new shoppinglist."""
+        serializer.save()
