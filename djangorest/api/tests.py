e7e88f58ed8e68793952d8dc5b10dcbfded5ae33
Kamara Deo <kamaradeo@Kamara-Deo.local>
2018-05-26 14:55:35 +0300

    [Feature) Initial setup

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-from .models import Bucketlist
+from .models import Shoppinglist
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
+
+
+
 # Create your tests here.
