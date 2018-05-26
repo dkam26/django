6003a666afe9279f62f61c5d433bbe24346a44ed
Kamara Deo <kamaradeo@Kamara-Deo.local>
2018-05-26 14:48:48 +0300

    [Feature) Initial setup

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db import models
 
+
+class User(models.Model):
+    user = models.CharField(max_length=255, blank=False, unique=True)
+    password = models.CharField(max_length=255, blank=False, unique=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
+
+
+class Shoppinglist(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
 # Create your models here.
---
+++ djangorest/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Shoppinglist
+
+class ShoppinglistSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Shoppinglist
+        fields = ('id', 'name', 'date_created')
+        read_only_fields = ('date_created')
