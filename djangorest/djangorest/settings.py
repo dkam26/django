c8eaa60e2e8f495aa1d320fcf3ca40791eb09b48
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-07-09 12:23:58 +0300

    [Chore] setup circleci

--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -83,9 +83,7 @@ DATABASES = {
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'TEST': {
-            'NAME': 'mytestdatabase',
-        },
+        
     }
 }
 
